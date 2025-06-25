02aa272982f66a42dd999e3d5dc9ba4f4c804675
skellgreco <tixisb@gmail.com>
2025-06-26 00:36:08 +0300

    Fixed redirect link

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -87,7 +87,7 @@ export default function Home() {
             </Button>
 
             <Button asChild>
-              <a href="./docs">
+              <a href="./guide">
                 Documentation
               </a>
             </Button>
@@ -164,7 +164,7 @@ export default function Home() {
           </Button>
 
           <Button asChild>
-              <a href="./docs">
+              <a href="./guide">
                 Documentation
               </a>
             </Button>
